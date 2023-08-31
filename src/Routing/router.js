72744c0b87442c35c92313d0b507738cfced114c
BohdanDem <bogdandemchuk.1@gmail.com>
2023-08-31 11:23:53 +0300

    init commit

---
+++ src/Routing/router.js
@@ -0,0 +1,26 @@
+import {createBrowserRouter} from "react-router-dom";
+import App from "../App";
+import UsersPage from "../pages/UsersPage";
+import PostsPage from "../pages/PostsPage";
+import CommentsPage from "../pages/CommentsPage";
+
+export const routers = createBrowserRouter([
+    {
+        path:'/',
+        element: <App/>,
+        children: [
+            {
+                path: 'users',
+                element: <UsersPage/>
+            },
+            {
+                path: 'posts',
+                element: <PostsPage/>
+            },
+            {
+                path: 'comments',
+                element: <CommentsPage/>
+            }
+        ]
+    }
+])
