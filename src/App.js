d9b76d33f740a48f227e508f86b355a6d13d7d4e
BohdanDem <bogdandemchuk.1@gmail.com>
2023-09-03 13:31:33 +0300

    part_1

--- src/App.js
+++ src/App.js
@@ -1,25 +1,56 @@
-import logo from './logo.svg';
-import './App.css';
+import {useReducer} from 'react';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+const initialState = {
+  count1: 0,
+  count2: 0
 }
 
-export default App;
+const reducer = (state, action) => {
+  switch (action.type) {
+    case 'INC_COUNT1':
+      state.count1++
+      break
+    case 'DEC_COUNT1':
+      state.count1 --
+      break
+    case 'RESET_COUNT1':
+      state.count1 = 0
+      break
+    case 'INC_COUNT2':
+      state.count2 ++
+      break
+    case 'DEC_COUNT2':
+      state.count2 --
+      break
+    case 'RESET_COUNT2':
+      state.count2 = 0
+      break
+    default:
+      return state
+  }
+  return {...state}
+}
+
+const App = () => {
+
+  const [state, dispatch] = useReducer(reducer, initialState)
+
+  return (
+      <div>
+        <div>
+          count_1: {state.count1}
+          <button onClick={() => dispatch({type: 'INC_COUNT1'})}>inc</button>
+          <button onClick={() => dispatch({type: 'DEC_COUNT1'})}>dec</button>
+          <button onClick={() => dispatch({type: 'RESET_COUNT1'})}>res</button>
+        </div>
+        <div>
+          count_2: {state.count2}
+          <button onClick={() => dispatch({type: 'INC_COUNT2'})}>inc</button>
+          <button onClick={() => dispatch({type: 'DEC_COUNT2'})}>dec</button>
+          <button onClick={() => dispatch({type: 'RESET_COUNT2'})}>res</button>
+        </div>
+      </div>
+  );
+};
+
+export {App};
